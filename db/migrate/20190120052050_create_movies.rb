4e87665b02844b311517b7706be939b832154ef8
Sean Langton <slangton1@gmail.com>
2019-01-20 05:30:12 +0000

    Automatically backed up by Learn

--- db/migrate/20190120052050_create_movies.rb
+++ db/migrate/20190120052050_create_movies.rb
@@ -1,9 +1,10 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
   def change
-    add_column :movies, :title, :string
-    add_column :movies, :release_date, :integer
-    add_column :movies, :director, :string
-    add_column :movies, :lead, :string
-    add_column :movies, :in_theaters, :boolean
+    create_table :movies do |t|
+    t.string :title
+    t.integer :release_date
+    t.string :director
+    t.string :lead
+    t.boolean :in_theaters
   end
 end
